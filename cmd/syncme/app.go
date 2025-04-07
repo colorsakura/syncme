f577010afc8b24e4f9cef4303f8da35d84d71895
iFlygo <iflygo@outlook.com>
2025-04-07 10:26:12 +0800

    Update main.go

---
+++ cmd/syncme/app.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"log"
+	"sync"
+	"time"
+
+	"github.com/colorsakura/syncme/internal/discover"
+	"github.com/colorsakura/syncme/internal/protocol"
+	"github.com/thejerf/suture/v4"
+)
+
+type App struct {
+	mainService       *suture.Supervisor
+	mainServiceCancel context.CancelFunc
+	stopOnce          sync.Once
+	stopped           chan struct{}
+}
+
+func NewApp() (*App, error) {
+	app := &App{
+		stopped: make(chan struct{}),
+	}
+	close(app.stopped) // Hasn't been started, so shouldn't block on Wait.
+	return app, nil
+}
+
+func (app *App) Start() error {
+	app.mainService = suture.New("main", suture.Spec{})
+
+	app.stopped = make(chan struct{})
+	ctx, cancel := context.WithCancel(context.Background())
+	errChan := app.mainService.ServeBackground(ctx)
+
+	app.mainServiceCancel = cancel
+
+	go app.wait(errChan)
+
+	uid, _ := protocol.NewDeviceID([]byte{})
+
+	discoveryManager := discover.NewManager(uid, []string{}, log.Default())
+
+	app.mainService.Add(discoveryManager)
+
+	return nil
+}
+
+func (a *App) wait(errChan <-chan error) {
+	<-errChan
+
+	done := make(chan struct{})
+	go func() {
+		close(done)
+	}()
+	select {
+	case <-done:
+	case <-time.After(10 * time.Second):
+	}
+
+	close(a.stopped)
+}
+
+func (app *App) Wait() {
+	<-app.stopped
+}
